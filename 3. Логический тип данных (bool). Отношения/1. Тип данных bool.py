c5b43d259647172af52e862b2eee3a9208f8539a
iprabbit <ilya.pechenkov@gmail.com>
2022-09-29 21:06:30 +0300

    Логический тип данных (bool). Отношения

---
+++ 3. Логический тип данных (bool). Отношения/1. Тип данных bool.py
@@ -0,0 +1,53 @@
+# boolean
+# bool
+
+# True (1) — истина
+# False (0) — ложь
+
+# print(3 > 5)
+
+# and (и) — конъюнкция (логические умножение):
+# используется для того, чтобы записать несколько условий,
+# которые должны выполняться ОДНОВРЕМЕННО
+# ВСЕ условия истинны
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# or (или) — дизъюнкция (логическое сложение):
+# используется для того, чтобы записать несколько ОПЦИОНАЛЬНЫХ условий
+# ХОТЯ БЫ ОДНО условие истинно
+# print(False or False)
+# print(False or True)
+# print(True or False)
+# print(True or True)
+
+# x = 5
+# print(x == 2 or x == 4 or x == 5)
+
+# not (не) — отрицание (инверсия):
+# используется для того, чтобы инвертировать условие
+
+# print(not True)
+# print(not False)
+
+# 1. not
+# 2. and
+# 3. or
+
+# print(True and False or True)
+# print(False and True or False)
+# print(False or True and not False)
+
+# print(True + True)
+# print(True + False + False + True)
+# print(True * True)
+
+x = 5
+print(3 * x > 12)
+print((3 * x) > 12)
+print(3 * (x > 12))
+
+# and, or — ленивые!
